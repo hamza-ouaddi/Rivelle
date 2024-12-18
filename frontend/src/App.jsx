1b875a7a6ac37c2b2aae473647bba10a8d4fdaff
hamza-ouaddi <hamza0addi@gmail.com>
2024-12-18 23:23:34 +0100

    initial commit with base project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="font-clashDisplay text-8xl font-semibold">Rivelle</h1>
+    </>
+  );
+}
+
+export default App;
