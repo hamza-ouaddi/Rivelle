5a8aa1086a3e5c1717c8da63e843c1cefe3e237e
hamza-ouaddi <hamza0addi@gmail.com>
2024-12-23 13:40:44 +0100

    add features section to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/home_page/HeroSection";
+import FeaturesSection from "../components/home_page/FeaturesSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturesSection />
     </main>
   );
 }
