c154a6e76fcefc5c0f77222642727f0d7532580e
hamza-ouaddi <hamza0addi@gmail.com>
2025-01-23 23:57:45 +0100

    implement JWT token generation and verification

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import { generateToken } from "../utils/auth.utils.js";
 
 export const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
@@ -38,7 +39,26 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    res.status(200).json({ message: "Login successfully", user });
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({
+      message: "Login successfully",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        profilePicture: user.profilePicture,
+        bio: user.bio,
+      },
+    });
   } catch (error) {
     console.error("Login error: ", error.message);
     res.status(500).json({ message: "Error Logged in user." });
